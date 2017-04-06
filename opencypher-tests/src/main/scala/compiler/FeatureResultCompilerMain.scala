30b4ec940d5f3bb145b9e505ffbfbf4c3e9dce1f
zsamboki <zsambeek.andras@gmail.com>
2017-04-06 21:42:42 +0200

    refactor code

--- opencypher-tests/src/main/scala/compiler/FeatureResultCompilerMain.scala
+++ opencypher-tests/src/main/scala/compiler/FeatureResultCompilerMain.scala
@@ -1,23 +1,49 @@
 package compiler
 
-import ingraph.cucumber.featureresult.parser.FeatureParser
+import neo4j.driver.testkit.EmbeddedTestkitDriver
+import org.neo4j.driver.internal.value.NodeValue
+import org.neo4j.driver.v1.{Record, StatementResult, Value}
 
+import scala.collection.JavaConverters._
+import scala.collection.mutable
 object FeatureResultCompilerMain {
 
   def main(args: Array[String]): Unit = {
-    val s1 = "<({weight: 3})-[:REL {something: 'ab'}]->(:Label2:Label3 {name: 'hello'})>"
+    /*val s1 = "<({weight: 3})-[:REL {something: 'ab'}]->(:Label2:Label3 {name: 'hello'})>"*/
     val s2 = "2"
-    val s3 = "[(:Label2:Label3 {name: 'hello'})]"
+   /* val s3 = "[(:Label2:Label3 {name: 'hello'})]"
     val s4 = "['a', ['b']]"
     val s5 = "(:LABEL {attr: ['a', ['b']]})"
-    val s6 = "<()-[:R1]->()>"
+    val s6 = "<()-[:R1]->()>"*/
     
-    println(FeatureResultCompiler.parseAndCompile(s1).getClass())
+   /* println(FeatureResultCompiler.parseAndCompile(s1).getClass())
     println(FeatureResultCompiler.parseAndCompile(s2).getClass())
     println(FeatureResultCompiler.parseAndCompile(s3).getClass())
     println(FeatureResultCompiler.parseAndCompile(s4).getClass())
-    println(FeatureResultCompiler.parseAndCompile(s5).getClass())
-    println(FeatureResultCompiler.parseAndCompile(s6).getClass())
+    println(FeatureResultCompiler.parseAndCompile(s5).getClass())*/
+    println(FeatureResultCompiler.parseAndCompile(s2).getClass())
+    val expectedResultString = "(:L)"
+    var expectedResultValue = FeatureResultCompiler.parseAndCompile(expectedResultString)
+    var driver = new EmbeddedTestkitDriver
+    driver.session.run("CREATE (a:L), (b1), (b2)\n      CREATE (a)-[:A]->(b1), (a)-[:A]->(b2)")
+    driver.session.beginTransaction
+    var actualResultBuffer = driver.session.run("MATCH p=(a:L)-[*]->(b)\n      RETURN b, avg(length(p))").list.asScala
+    resultEqualityCheck(expectedResultValue, actualResultBuffer )
   }
-  
+
+
+  def resultEqualityCheck(expectedResult: Value, actualStatementResultBuffer: mutable.Buffer[Record] ): Unit ={
+   //TODO how to check if i already found an element before
+    /*
+    properties --> asEntity.asMap
+    labels
+     */
+    println(expectedResult)
+    expectedResult match{
+      case nodeValue:NodeValue => {
+          println(nodeValue.asEntity.labels().iterator().next())
+      }
+    }
+    println(actualStatementResultBuffer)
+  }
 }
--- opencypher-tests/src/test/scala/calculator/QueryCalculator.scala
+++ opencypher-tests/src/test/scala/calculator/QueryCalculator.scala
@@ -1,14 +1,10 @@
 package calculator
 
 import cucumber.api.DataTable
-import neo4j.driver.reactive.impl.Neo4jReactiveSession
-import neo4j.driver.reactive.interfaces.ReactiveSession
-import neo4j.driver.testkit.EmbeddedTestkitSession
-import org.neo4j.cypher.SyntaxException
-import org.neo4j.driver.v1.Session
+import org.neo4j.driver.v1.{Session}
 
-import scala.collection.JavaConversions._
 import scala.collection.mutable.ListBuffer
+import collection.JavaConverters._
 
 /**
   * Created by Andras Zsamboki on 2017.03.01..
@@ -16,104 +12,72 @@ import scala.collection.mutable.ListBuffer
 object QueryCalculator {
 
   def calculateSideEffects(session: Session, query: String): DatabaseResult = {
-    var sideEffectsMap = scala.collection.mutable.Map[String, Int]()
-    val startNodes = session.run("MATCH (n) RETURN count(n) AS count").next().get("count").asInt
-    val startRelationships = session.run("MATCH ()-[r]-() RETURN count(DISTINCT r) AS count").next().get("count").asInt
-    val startLabels = session.run("MATCH (n) UNWIND labels(n) AS label\nMATCH (n) WHERE label IN labels(n) RETURN count(DISTINCT label) AS count").next().get("count").asInt
-    val startProperties = session.run("MATCH (n) RETURN count(properties(n)) AS count").next().get("count").asInt
+
+    val sideEffectsMap = scala.collection.mutable.Map[String, (String, Int)]()
+    sideEffectsMap += "nodes" -> ("MATCH (n) RETURN count(n) AS count", 0)
+    sideEffectsMap += "relationships" -> ("MATCH ()-[r]-() RETURN count(DISTINCT r) AS count", 0)
+    sideEffectsMap += "labels" -> ("MATCH (n) UNWIND labels(n) AS label\nMATCH (n) WHERE label IN labels(n) RETURN count(DISTINCT label) AS count", 0)
+    sideEffectsMap += "properties" -> ("MATCH (n) RETURN count(properties(n)) AS count", 0)
+    sideEffectsMap.foreach(x =>
+      sideEffectsMap(x._1) = (x._2._1, session.run(x._2._1).next().get("count").asInt())
+    )
+
     session.beginTransaction()
     val sessionResult = session.run(query)
-    val diffNodes = session.run("MATCH (n) RETURN count(n) AS count").next().get("count").asInt - startNodes
-    val diffRelationships = session.run("MATCH ()-[r]-() RETURN count(DISTINCT r) AS count").next().get("count").asInt - startRelationships
-    val diffLabels = session.run("MATCH (n) UNWIND labels(n) AS label\nMATCH (n) WHERE label IN labels(n) RETURN count(DISTINCT label) AS count").next().get("count").asInt - startLabels
-    val diffProperties = session.run("MATCH (n) RETURN count(properties(n)) AS count").next().get("count").asInt - startProperties
+    
+    sideEffectsMap.foreach(x => {
+      val diff = session.run(x._2._1).next().get("count").asInt() - x._2._2
 
-    if(diffNodes != 0){
-      if(diffNodes>0){
-        sideEffectsMap+=Tuple2("+nodes", diffNodes)
-      }else{
-        sideEffectsMap+=Tuple2("-nodes", diffNodes)
+      diff compare 0 match {
+        case 1 => sideEffectsMap(x._1) = ("+" + x._1, diff)
+        case -1 => sideEffectsMap(x._1) = ("-" + x._1, -diff)
+        case 0 => sideEffectsMap -= x._1
       }
     }
+    )
 
-    if(diffRelationships != 0){
-      if(diffRelationships>0){
-        sideEffectsMap+=Tuple2("+relationships",diffRelationships)
-      }else{
-        sideEffectsMap+=Tuple2("-relationships", diffRelationships)
-      }
-    }
 
-    if(diffLabels != 0){
-      if(diffLabels>0){
-        sideEffectsMap+=Tuple2("+labels", diffLabels)
-      }else{
-        sideEffectsMap+=Tuple2("-labels", diffLabels)
-      }
-    }
-
-    if(diffProperties != 0){
-      if(diffProperties>0){
-        sideEffectsMap+=Tuple2("+properties", diffProperties)
-      }else{
-        sideEffectsMap+=Tuple2("-properties", diffProperties)
-      }
-    }
-
-    var unProcessedQueryResultsBuffer = asScalaBuffer(sessionResult.list())
-    var queryResultsBuffer = ListBuffer[ListBuffer[String]]()
+    val unProcessedQueryResultsBuffer = sessionResult.list().asScala
+    val queryResultsBuffer = ListBuffer[ListBuffer[String]]()
 
     if (unProcessedQueryResultsBuffer.nonEmpty) {
-      var tmp = ListBuffer[String]()
-      tmp ++= unProcessedQueryResultsBuffer.head.keys()
-      queryResultsBuffer += tmp
+      queryResultsBuffer :+ unProcessedQueryResultsBuffer.head.keys().asScala
     }
-    for (i <- unProcessedQueryResultsBuffer.indices) {
-      var tmp = ListBuffer[String]()
-      var valueBuffer = ListBuffer[String]()
-      for (value <- unProcessedQueryResultsBuffer(i).values()) {
-        valueBuffer += value.toString
-      }
-      tmp ++= valueBuffer
-      queryResultsBuffer += tmp
-    }
-    DatabaseResult(queryResultsBuffer, sideEffectsMap)
+
+    unProcessedQueryResultsBuffer.foreach(x =>
+      queryResultsBuffer :+ x.values().asScala.map(_.toString)
+    )
+
+    DatabaseResult(queryResultsBuffer, sideEffectsMap.map(x => x._2._1 -> x._2._2))
 
   }
 
   def checkResultEqualiy(result: DatabaseResult, dataTable: DataTable): Boolean = {
-    var expectedResult = ListBuffer[ListBuffer[String]]()
-    for (rows <- dataTable.getGherkinRows) {
-      var tmp = ListBuffer[String]()
-      for (elem <- rows.getCells) {
-        tmp += elem.replace(''', '"')
-      }
-      expectedResult += tmp
-    }
+    val expectedResult = ListBuffer[ListBuffer[String]]()
+
+    dataTable.getGherkinRows.asScala.foreach(x =>
+      expectedResult :+ x.getCells.asScala.map(x => x.replace(''', '"'))
+    )
     println("EXPECTED RESULT: ")
     expectedResult.foreach(println)
     println("QUERY RESULT: ")
     result.queryResult.foreach(println)
 
-    for (i <- result.queryResult.indices) {
-      result.queryResult(i) = result.queryResult(i).map(_.toUpperCase)
-    }
+    result.queryResult.foreach(_.map(_.toUpperCase))
+    expectedResult.foreach(_.map(_.toUpperCase))
 
-    for (i <- expectedResult.indices) {
-      expectedResult(i) = expectedResult(i).map(_.toUpperCase)
-    }
-    var isSame = false
-    for (i <- expectedResult.indices) {
-      isSame = false
-      for (j <- result.queryResult.indices) {
-        if (expectedResult(i).size == expectedResult(i).intersect(result.queryResult(j)).size) {
+    expectedResult.foreach(x => {
+      var isSame = false
+      result.queryResult.foreach(y => {
+
+        if (x.size == x.intersect(y).size) {
           isSame = true
         }
       }
-      if (!isSame) {
-        return false
-      }
-    }
+      )
+      if (!isSame) return false
+
+    })
     true
   }
 
@@ -124,12 +88,12 @@ object QueryCalculator {
     result.sideEffect.foreach(println)
     expectedResult match {
       case Some(x) =>
-        for (i <- 0 until x.raw().size()) {
-          expectedResultMap += (x.raw.get(i).get(0) -> x.raw().get(i).get(1).toInt)
-        }
+        x.raw().asScala.foreach(x =>
+          expectedResultMap += (x.get(0) -> x.get(1).toInt)
+        )
         println("expected result: ")
         expectedResultMap.foreach(println)
-        result.sideEffect.containsAll(expectedResultMap)
+        result.sideEffect == expectedResultMap
       case None => result.sideEffect.isEmpty
     }
   }
